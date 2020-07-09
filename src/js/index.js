80b58efa21f4c8ed7c05843416aaf87ae53778f2
Ihor Burenko <burenko@gmail.com>
2020-07-09 18:54:31 +0300

    feat: add JS animations

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
+import './../assets/js/script'
+import {
+  headerNavMenu,
+  burgerBtn,
+  navMapsBtn,
+  closeBurgerMenu,
+  openBurgerMenu,
+  toggleDropdownMaps, moveMottoTextOnLoad
+} from '../assets/js/script';
 
 window.onload = function () {
-
+  navMapsBtn.addEventListener('click', toggleDropdownMaps);
+  burgerBtn.addEventListener('click', openBurgerMenu);
+  headerNavMenu.addEventListener('click', closeBurgerMenu);
+  setTimeout(moveMottoTextOnLoad, 500);
 };
